55682adda403f934eb12de1cc4f4e17b7d2fd510
Moosa <moosa.esfahanian@pearson.com>
2013-09-07 11:49:12 -0500

    initial copy web

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+(function(){
+    var headerTmpl = _.template(get('./template/header'))();
+    var footerTmpl = _.template(get('./template/footer-contact'))();
+    $('#header').html(headerTmpl);
+    $('#footer').html(footerTmpl);
+
+    ActiveHeader();
+})();
+
+
+ function get(url) {
+    var data = "<h1> failed to load url : " + url + "</h1>";
+    $.ajax({
+        async: false,
+        dataType: "text",
+        url: url,
+        success: function(response) {
+            data = response;
+        }
+    });
+    return data;
+}
+
+function ActiveHeader(){
+          var currentUrl = window.location.href.toString();
+    $('#header').find('.active').removeClass('active');
+
+    if(currentUrl.indexOf('about')!== -1){
+         $('#aboutUsMenu').addClass('active');
+    }
+    else if (currentUrl.indexOf('features')!== -1 || currentUrl.indexOf('factory')!== -1) {
+        $('#picMenu').addClass('active');
+
+    }else if (currentUrl.indexOf('products')!== -1 ) {
+        $('#productsMenu').addClass('active');
+
+    }else if (currentUrl.indexOf('contact')!== -1) {
+        $('#contactUsMenu').addClass('active');
+
+    } else{
+        $('#homeMenu').addClass('active');
+    }
+
+
+}
